0e8d7f5a28aedacc9aeb14b1774d480217ad532b
Jesper Kling <63609498+jesperkling@users.noreply.github.com>
2022-04-09 02:08:58 +0200

    added models and removed example.js.

---
+++ models/Album.js
@@ -0,0 +1,19 @@
+/**
+ * Album model
+ */
+
+module.exports = (bookshelf) => {
+	return bookshelf.model('Album', {
+		tableName: 'albums',
+        photos() {
+            return this.belongsToMany('Photo');
+        }, 
+        users() {
+            return this.belongsTo('User');
+        }
+	}, {
+        async fetchById(id, fetchOptions = {}) {
+            return await new this({ id }).fetch(fetchOptions);
+        }
+    });
+};
---
+++ models/User.js
@@ -0,0 +1,35 @@
+/**
+ * User model
+ */
+const bcrypt = require('bcrypt');
+
+module.exports = (bookshelf) => {
+	return bookshelf.model('User', {
+		tableName: 'users',
+        photos() {
+            return this.hasMany('Photo');
+        }, 
+        albums() {
+            return this.hasMany('Album');
+        }
+	}, {
+        async login(email, password) {
+            const user = await new this({ email }).fetch({ require: false });
+            if(!user) {
+                return false;
+            }
+
+            const hash = user.get('password');
+
+            const result = await bcrypt.compare(password, hash);
+            if (!result) {
+                return 'Wrong password', false;
+            }
+
+            return user;
+        }, 
+        async fetchById(id, fetchOptions = {}) {
+            return await new this({ id }).fetch(fetchOptions);
+        }
+    });
+};
