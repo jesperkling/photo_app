fe17846c7fd878420eb99bab319eaebd887d89b9
Jesper Kling <63609498+jesperkling@users.noreply.github.com>
2022-04-09 02:12:52 +0200

    added middlewares folder with auth.js.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middlewares/auth');
 const userController = require('../controllers/user_controller');
 const userValidationRules = require('../validation/user');
 
