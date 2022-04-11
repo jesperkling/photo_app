4253e40e1d1166ebf46947dedab8607ce6231e44
Jesper Kling <63609498+jesperkling@users.noreply.github.com>
2022-04-11 17:32:25 +0200

    changed routes to plural form.

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,8 @@ router.get('/', (req, res, next) => {
 	res.send({ success: true, data: { msg: 'We now have a connection' }});
 });
 
-router.use('/album', auth.basic, require('./album'));
-router.use('/photo', auth.basic, require('./photo'));
+router.use('/album', auth.basic, require('./albums'));
+router.use('/photo', auth.basic, require('./photos'));
 router.post('/register', userValidationRules.createRules, userController.register);
 
 module.exports = router;
